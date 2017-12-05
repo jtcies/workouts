b1863a59c59a3fd6573ed6c18b5a73bf44575478
Joseph Ciesielski <jtcies@gmail.com>
2017-12-04 22:12:37 -0500

    adding pace and overview pages

--- R/app.R
+++ R/app.R
@@ -5,13 +5,11 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(forcats) 
+library(stringr)
 
 source('../common/jtc_theme.R')
 
 # ----- functions and ref data
-# all dates from 2017-06-01 for charts
-
-dates <- data.frame(date = seq(as.Date('2017-06-01'), today(), by = '1 day'))
 
 NA_to_none <- function(x) {
   # changes NAs to 'none', use with mutate_at
@@ -19,67 +17,75 @@ NA_to_none <- function(x) {
   x
 }
 
-# limitsa for time of day charts
-lims <- as.POSIXct(strptime(
-  c(paste0(today(), " 00:01"), paste0(today(), " 23:59")), 
-  format = "%Y-%m-%d %H:%M"))
+calc_pace <- function(dist, time) {
+  # takes the time of the workout and converts it to a duration object
+  # for calcuating pace
+  split <- str_split(time, ":")
+  split <- lapply(split, as.numeric)
+  seconds <- lapply(split, function(x) {
+    x[1] * 3600 + x[2] * 60 + x[3]
+  })
+  dist / unlist(seconds) * 3600
+}
+  
 
 # ----- read data -----
 
 cardio <- read_csv('../data/bike_run_log.csv')
-strength <- read_csv('../data/strength_log.csv')
 
+
+# ---- tidy and transform ---
+# create a pace variable
+
+cardio$pace <- calc_pace(cardio$distance, cardio$time)
 
 # ----- shiny -----
 
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("JTC workout data"),
-   
-   # Sidebar with input for chart types 
-   sidebarLayout(
-      sidebarPanel(
-         selectInput('option', NULL,
-                     c('date and time' = 'date.time',
-                       'test' = 'test'))
-      ),
-      mainPanel(
-         plotOutput(outputId = "plot")
-      )
-   )
+  
+  # Application title
+  titlePanel("JTC workout data"),
+  
+  # Sidebar with input for chart types 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("option",
+                  "page",
+                  c('overview',
+                    'pace')
+                  )
+    ),
+    mainPanel(
+      plotOutput(outputId = "plot")
+    )
+  )
 )
 
 # return the plot that was chosen
 server <- function(input, output) {
   
-   output$plot <- renderPlot( {
-     if(input$option == 'date.time') {
-       
-       plot <- strength %>% 
-         mutate(type = 'strength') %>% 
-         bind_rows(cardio) %>% 
-         distinct(date, start, type) %>% 
-         mutate(type = fct_relevel(type, 'bike', 'run', 'strength'),
-                start = as.POSIXct(strptime(
-                  paste0(today(), ' ', start), 
-                  format = "%Y-%m-%d %H:%M"))) %>% 
-           ggplot(aes(date, start, colour = type)) +
-          geom_point(size = 2) +
-          scale_y_datetime(limits = lims, date_labels = '%H:%M') +
-          labs(y = 'start time') +
-          jtc
-     }
-     
-     if(input$option == 'test') {
-         plot <- ggplot(mtcars, aes(x = cyl, y = mpg)) + geom_point()
-       }
-       
-       plot
-       
-     })
+  output$plot <- renderPlot({
+    if(input$option == 'overview') {
+      
+      plot <- ggplot(cardio, aes(x = date)) +
+        geom_histogram(binwidth = 7) +
+        labs(x = "week") + 
+        jtc
+      
+    }
+    
+    if(input$option == 'pace') {
+      plot <- ggplot(cardio, aes(x = date, y = pace, colour = type)) + 
+        geom_line() +
+        geom_point(size = 2) + 
+        jtc
+    }
+    
+    plot
+    
+  })
 }
-   
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
